544246f3904e61e72ccde923a1fb73390986dad0
Anna <azeleranskaya@gmail.com>
2024-11-19 00:27:23 +0300

    update category.py and product.py

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     name: str
     description: str
@@ -8,6 +11,24 @@ class Category:
     def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+
+    def add_product(self, product: Product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+
+    @property
+    def products(self):
+        product_str = ""
+        for product in self.__products:
+            product_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт."
+        return product_str
+
+
+    # @add_product.setter
+    # def add_product(self, name, description, price, quantity):
+    #     Product(name, description, price, quantity)
--- src/product.py
+++ src/product.py
@@ -7,5 +7,23 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+
+    @classmethod
+    def new_product(cls, product: dict):
+        return cls(**product)
+
+    @property
+    def price(self):
+        return self.__price
+
+
+    @price.setter
+    def price(self, new_price: float):
+        if new_price > 0:
+            self.__price = new_price
+        else:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
