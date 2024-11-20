eece793409e6fd17a83182df29fd281f5e7f4bc5
Anna <azeleranskaya@gmail.com>
2024-11-20 23:41:54 +0300

    add tests for magic methods in product and category

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import pytest
 
 from src.category import Category
@@ -5,24 +7,24 @@ from src.product import Product
 
 
 @pytest.fixture
-def first_product():
+def first_product() -> Any:
     return Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
 
 @pytest.fixture
-def second_product():
+def second_product() -> Any:
     return Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
 
 
 @pytest.fixture
-def first_category(first_product, second_product):
+def first_category(first_product: Any, second_product: Any) -> Any:
     return Category("Смартфоны", "Смартфоны, как средство не только коммуникации.", [first_product, second_product])
 
 
 @pytest.fixture
-def second_category(first_product, second_product):
+def second_category(first_product: Any, second_product: Any) -> Any:
     return Category(
         "Телевизоры",
         "Комфорт и эстетика.",
-        [first_product, second_product, Product("Пылесос Xiaomi", "Dreame v12", "20999.99", 2)],
+        [first_product, second_product, Product("Пылесос Xiaomi", "Dreame v12", 20999.99, 2)],
     )
--- tests/test_category.py
+++ tests/test_category.py
@@ -1,4 +1,7 @@
-def test_category(first_category, second_category):
+from typing import Any
+
+
+def test_category(first_category: Any, second_category: Any) -> None:
     assert first_category.name == "Смартфоны"
     assert first_category.description == "Смартфоны, как средство не только коммуникации."
     assert len(first_category.products_in_list) == 2
@@ -13,13 +16,17 @@ def test_category(first_category, second_category):
     assert second_category.product_count == 5
 
 
-def test_category_products_property(first_category):
+def test_category_products_property(first_category: Any) -> None:
     assert first_category.products == (
         "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.\n" "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n"
     )
 
 
-def test_category_add_product(second_category, first_product):
+def test_category_add_product(second_category: Any, first_product: Any) -> None:
     assert len(second_category.products_in_list) == 3
     second_category.add_product(first_product)
     assert len(second_category.products_in_list) == 4
+
+
+def test_category_str(second_category: Any) -> None:
+    assert str(second_category) == "Телевизоры, количество продуктов: 24 шт."
